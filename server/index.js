079f2afcc2864e9d5ec7f831d91909691277f195
Oleg Sucharevich <oleg2807@gmail.com>
2021-02-26 16:37:58 +0200

    wip

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 app.use(morgan('tiny'))
 
